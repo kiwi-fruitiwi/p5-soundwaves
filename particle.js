f1c69eb88ad10d75658e68a2da34fd133074d608
kiwi-fruitiwi <zane.tian@gmail.com>
2021-10-04 12:38:10 -0700

    add sinusoidal motion based on pos.x of each particle

--- particle.js
+++ particle.js
@@ -1,15 +1,32 @@
 class Particle {
-    constructor(x, y) {
+    constructor(x, y, phase) {
         this.pos = new p5.Vector(x, y)
         this.vel = new p5.Vector()
         this.acc = new p5.Vector()
-        this.r = 3
+        this.phase = phase
+        this.r = 2
+
+        this.originalx = this.pos.x
     }
 
     update() {
         this.vel.add(this.acc)
         this.pos.add(this.vel)
         this.acc.setMag(0)
+
+        // gently oscillate our particle in the x direction
+        // this our our period. 100 means 100 pixels on the screen
+        let period = 100
+        let ω = TWO_PI/period
+
+        // we want to modify the amplitude of our sine wave to oscillate...
+        // like a sine wave. the magnitude of the amplitude should depend on
+        // the original x position of our particle
+        let amp = map(sin(ω*this.originalx), -1, 1, 0, 20)
+
+        // we want our particle to oscillate around a fixed point, i.e. its
+        // original position
+        this.pos.x = this.originalx + amp*sin(frameCount/30)
     }
 
     show() {
--- sketch.js
+++ sketch.js
@@ -9,8 +9,11 @@ these?
 coding plans:
 .   create particle class
 .   create grid of particles
-    assign varying sinusoidal motion to every particle based on its position.x
-
+.   assign varying sinusoidal motion to every particle based on its position.x
+    optimize by creating an array of particles and just displaying copies of
+        them through some height instead of doing calculations on every
+        particle. this is possible because every particle follows the particle
+        above and below.
  */
 
 let font
@@ -25,12 +28,12 @@ function setup() {
     colorMode(HSB, 360, 100, 100, 100)
     particles = []
 
+    // create a grid of particles!
     let PADDING = 40
-    let SPACING = 15
-    // create a grid of particles
+    let SPACING = 8
     for (let i=PADDING; i<=width-PADDING; i+=SPACING) {
-        for (let j=PADDING; j<=height-PADDING; j+=SPACING) {
-            particles.push(new Particle(i, j))
+        for (let j=height/2-50; j<=height/2+50; j+=SPACING) {
+            particles.push(new Particle(i, j, 0))
         }
     }
 }
@@ -40,4 +43,6 @@ function draw() {
 
     particles.forEach(p => p.update())
     particles.forEach(p => p.show())
+
+    // apply sinusoidal motion to each particle in the x direction
 }
