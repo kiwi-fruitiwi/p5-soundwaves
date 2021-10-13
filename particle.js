183e446f38c88e54395826e47fdc20781f93180e
kiwi-fruitiwi <zane.tian@gmail.com>
2021-10-13 12:24:56 -0700

    add single-particle oscillation

--- particle.js
+++ particle.js
@@ -3,10 +3,14 @@ class Particle {
         this.pos = new p5.Vector(x, y)
         this.r = 3
 
+        // we have to save the original x position because we'll be moving
+        this.initialX = x
+
         // each particle exhibits sinusoidal motion
         this.amp = 100
         this.period = 100
         this.angle = 0
+        this.phase = 0
 
         // particles don't move until they are activated
         this.activated = false
@@ -31,7 +35,15 @@ class Particle {
                 // what happens if this particle is activated and the delay
                 // period is finished
 
-                
+                // sine waves are in the general form y=a*sin(ω*(x-c))+d
+                //    ω is the angular velocity, a is amplitude
+                //    ω*period is always 2π as evidenced by cosine and sine
+                let ω = TAU/this.period
+
+                this.pos.x = this.initialX +
+                    this.amp*sin(ω*(this.angle-this.phase))
+
+                this.angle += 0.03
             }
         }
     }
--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@
 plan
     create particle class: update, show, activate
     make one oscillate with delay 0
+        🐞 called particle.activate with no arguments :P
+        15min
     fiddle with delay or phase
     add grid of particles
     highlighted row +marker
@@ -14,6 +16,8 @@ let font
 
 let ROWS = 18
 let COLS = 55
+let SPACING = 15
+let PADDING = 50
 
 let test
 let particles = []
@@ -25,6 +29,17 @@ function preload() {
 function setup() {
     createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
+    frameRate(144)
+
+    // let row
+    // for (let r=0; r<ROWS; r++) {
+    //     row = []
+    //     for (let c=0; c<COLS; c++) {
+    //        row.push(new Particle(
+    //            SPACING*c,
+    //            SPACING*r))
+    //     }
+    // }
 
     test = new Particle(width/2, height/2)
 }
@@ -34,4 +49,16 @@ function draw() {
 
     test.update()
     test.show()
+}
+
+function start() {
+    test.activate(
+        25, // amplitude
+        7, // period
+        0 // delay
+    )
+}
+
+function mousePressed() {
+    start()
 }
